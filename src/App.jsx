d6f1031f84109009a89e94ecdf0f1383e94c303f
Thignvs <thiagoneves44@gmail.com>
2023-07-03 18:36:31 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
-// import { Outlet } from 'react-router-dom'
-// import NavBar from './components/NavBar'
-// import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
-
-// function App() {
-
-//   return (
-//     <>
-//         <nav>
-//           <NavBar />
-//         </nav>
-//         <Outlet />
-//       <footer>
-//         <Footer />
-//       </footer>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-import React from 'react';
-import { Outlet, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
 
 function App() {
+
   return (
     <>
-      <nav>
-        <NavBar />
-      </nav>
-      <main>
-        <Route>
-          <Outlet />
-        </Route>
-      </main>
+        <nav>
+          <NavBar />
+        </nav>
+        <Outlet />
       <footer>
         <Footer />
       </footer>
     </>
-  );
+  )
 }
-export default App;
+
+export default App
+
+
+
+// import React from 'react';
+// import { Outlet, Route } from 'react-router-dom';
+// import NavBar from './components/NavBar';
+// import Footer from './components/Footer';
+
+// function App() {
+//   return (
+//     <>
+//       <nav>
+//         <NavBar />
+//       </nav>
+//       <main>
+//         <Route>
+//           <Outlet />
+//         </Route>
+//       </main>
+//       <footer>
+//         <Footer />
+//       </footer>
+//     </>
+//   );
+// }
+// export default App;
