ecd370cb4e47684032ded11831a52f20532c846b
Thignvs <thiagoneves44@gmail.com>
2023-07-03 16:43:45 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/scoot-multipage-website",
   plugins: [react()],
+  base: "/scoot-multipage-website",
 })
