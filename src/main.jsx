18119352852697f4567fcd708419b1c8cf87afe6
Thignvs <thiagoneves44@gmail.com>
2023-07-03 18:55:00 -0300

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -1,62 +1,62 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import About from './pages/About.jsx'
-// import Careers from './pages/Careers.jsx'
-// import Locations from './pages/Locations.jsx'
-// import Home from './pages/Home.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './pages/About.jsx'
+import Careers from './pages/Careers.jsx'
+import Locations from './pages/Locations.jsx'
+import Home from './pages/Home.jsx'
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Home />
-//       },
-//       {
-//         path: "/about",
-//         element: <About />
-//       }, {
-//         path: "/careers",
-//         element: <Careers />
-//       }, {
-//         path: "/locations",
-//         element: <Locations />
-//       },]
-//   },
-// ])
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>,
-// )
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import About from './pages/About';
-import Careers from './pages/Careers';
-import Locations from './pages/Locations';
-import Home from './pages/Home';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      }, {
+        path: "/careers",
+        element: <Careers />
+      }, {
+        path: "/locations",
+        element: <Locations />
+      },]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="careers" element={<Careers />} />
-          <Route path="locations" element={<Locations />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import App from './App';
+// import './index.css';
+// import About from './pages/About';
+// import Careers from './pages/Careers';
+// import Locations from './pages/Locations';
+// import Home from './pages/Home';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />}>
+//           <Route index element={<Home />} />
+//           <Route path="about" element={<About />} />
+//           <Route path="careers" element={<Careers />} />
+//           <Route path="locations" element={<Locations />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/scoot-multipage-website/",
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/scoot-multipage-website/'
+  }
+
+  return config
 })
