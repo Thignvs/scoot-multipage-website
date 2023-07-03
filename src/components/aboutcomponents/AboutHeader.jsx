1e17d995364d08f23f4fc22c5f5609469926158c
Thignvs <thiagoneves44@gmail.com>
2023-07-03 20:07:47 -0300

    test

--- src/components/aboutcomponents/AboutHeader.jsx
+++ src/components/aboutcomponents/AboutHeader.jsx
@@ -3,7 +3,7 @@ import whitecircles from '../../assets/patterns/white-circles.svg'
 
 const AboutHeader = () => {
     return (
-        <div className='py-20 relative bg-hero-bg flex justify-center md:justify-start items-center bg-no-repeat bg-cover bg-center md:bg-[url("./src/assets/images/about-hero-tablet.jpg")] lg:bg-[url("./src/assets/images/about-hero-desktop.jpg")] overflow-hidden'>
+        <div className='py-20 relative bg-hero-about-bg flex justify-center md:justify-start items-center bg-no-repeat bg-cover bg-center md:bg-hero-about-bg-tablet lg:bg-hero-about-bg-desktop overflow-hidden'>
             <h5 className='font-headings text-Snow text-4xl font-bold md:px-20'>About</h5>
             <img className='hidden md:block absolute -right-10' src={whitecircles} alt="whitecircleimage" />
         </div>
